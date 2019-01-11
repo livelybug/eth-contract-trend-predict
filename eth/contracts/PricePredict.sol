6f277cd9bae9bf967495739bd3a03855639849ad
burt <chensu@jinzhongkeji.com>
2019-01-11 16:10:53 +0800

    Add unit tests for the prediction contract; configure ganache's default block gas limit

--- eth/contracts/PricePredict.sol
+++ eth/contracts/PricePredict.sol
@@ -6,12 +6,12 @@ contract PricePredict {
     uint public minBet;
     uint public rangeMax;
     uint public poolSum;
-    address[] public storeAdds; 
-    
+    address[] public storeAdds;
+
     constructor(uint range, uint minimunt) public {
         require(range > 0);
         require(minimunt > 0);
-        
+
         manager = msg.sender;
         minBet = minimunt;
         rangeMax = range;
@@ -19,7 +19,7 @@ contract PricePredict {
         for(uint i = 0; i <= rangeMax; i++) {
             address betStorageAdd = new BetStorage(i);
             storeAdds.push(betStorageAdd);
-        }        
+        }
     }
 
     function bet(uint predict) public payable{
@@ -29,11 +29,11 @@ contract PricePredict {
         address storeAdd1 = storeAdds[predict];
         BetStorage betStorage1 = BetStorage(storeAdd1);
         if(betStorage1.betMapping(msg.sender) == 0) {
-           betStorage1.addBettor(msg.sender); 
+           betStorage1.addBettor(msg.sender);
         }
         betStorage1.addBetMapping(msg.sender, msg.value);
         betStorage1.addBetPool(msg.value);
-        
+
         poolSum += msg.value;
     }
 
@@ -43,23 +43,23 @@ contract PricePredict {
 
         address storeAdd2 = storeAdds[predict];
         BetStorage betStorage2 = BetStorage(storeAdd2);
-        require(betStorage2.betPool() > 0);        
+        require(betStorage2.betPool() > 0);
 
         // loop the bettors of winning group
         uint bettorsLen = betStorage2.getBettorsLen();
         for(uint i = 0; i < bettorsLen; i++ ) {
             address aWinner = betStorage2.bettors(i);
-            uint refund = betStorage2.betMapping(aWinner) / uint128(betStorage2.betPool()) * poolSum;
+            uint refund = betStorage2.betMapping(aWinner) / betStorage2.betPool() * poolSum;
             aWinner.transfer(refund);
         }
-        
+
         // reset
         storeAdds = new address[](0);
         for(i = 0; i <= rangeMax; i++) {
             address betStorageAdd = new BetStorage(i);
             storeAdds.push(betStorageAdd);
         }
-        
+
         poolSum = 0;
     }
 
@@ -67,7 +67,7 @@ contract PricePredict {
 
 
 contract BetStorage {
-    
+
     mapping(address => uint) public betMapping;
     address[] public bettors;
     uint public range;
@@ -82,15 +82,15 @@ contract BetStorage {
     function getBettorsLen() public view returns(uint) {
         return bettors.length;
     }
-    
+
     function addBettor(address bettor) public {
         bettors.push(bettor);
     }
-    
+
     function addBetMapping(address bettor, uint wager) public {
         betMapping[bettor] += wager;
     }
-    
+
     function addBetPool(uint wager) public {
         betPool += wager;
     }
---
+++ test/PricePredict.test.js
@@ -0,0 +1,100 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const blockGasLimit = 900000000;
+const options = { gasLimit: blockGasLimit };
+const web3 = new Web3(ganache.provider(options));
+
+const compiledBetStorage = require('../eth/build/:BetStorage.json');
+const compiledPricePredict = require('../eth/build/:PricePredict.json');
+
+let accounts = undefined;
+let predict = undefined;
+let maxRange = 2;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    let balance = await web3.eth.getBalance(accounts[0]);
+    console.log(balance);
+
+    predict = await new web3.eth.Contract(JSON.parse(compiledPricePredict.interface))
+        .deploy({
+            data: compiledPricePredict.bytecode,
+            arguments: [maxRange, 100]
+        })
+        .send({ from: accounts[0], gas: blockGasLimit });
+
+});
+
+describe('Predict Contract', () => {
+    it('deploy a predict contract', () => {
+        assert.ok(predict.options.address)
+    });
+
+    it('manager should be accout0', async () => {
+        const manager = await predict.methods.manager().call();
+        assert.equal(manager, accounts[0]);
+    });
+
+    it('verify storage max range', async () => {
+        const storeAdd = await predict.methods.storeAdds(maxRange).call();
+        const betStorage = await new web3.eth.Contract(
+            JSON.parse(compiledBetStorage.interface),
+            storeAdd
+        );
+        const _range = await betStorage.methods.range().call();
+        assert.equal(_range, maxRange);
+    });
+
+    it('bet on a prediction and verify the bet on storage', async () => {
+        await predict.methods.bet(maxRange).send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', "ether"),
+            gas: blockGasLimit
+        });
+
+        const storeAdd = await predict.methods.storeAdds(maxRange).call();
+        const betStorage = await new web3.eth.Contract(
+            JSON.parse(compiledBetStorage.interface),
+            storeAdd
+        );
+        const wager0 = await betStorage.methods.betMapping(accounts[0]).call();
+        assert.equal(wager0, web3.utils.toWei('0.02', "ether"));
+
+        const bettor = await betStorage.methods.bettors(0).call();
+        assert.equal(bettor, accounts[0]);
+    });
+
+    it('verify winner\' balance after resolving', async () => {
+        let balanceBefore = await web3.eth.getBalance(accounts[0]);
+
+        await predict.methods.bet(maxRange).send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', "ether"),
+            gas: blockGasLimit
+        });
+
+        await predict.methods.bet(maxRange - 1).send({
+            from: accounts[1],
+            value: web3.utils.toWei('0.3', "ether"),
+            gas: blockGasLimit
+        });
+
+        await predict.methods.bet(maxRange - 2).send({
+            from: accounts[2],
+            value: web3.utils.toWei('0.4', "ether"),
+            gas: blockGasLimit
+        });
+
+        await predict.methods.resolve(maxRange).send({
+            from: accounts[0],
+            gas: blockGasLimit
+        });
+
+
+        let balanceAfter = await web3.eth.getBalance(accounts[0]);
+
+        assert( (balanceAfter - balanceBefore) > web3.utils.toWei('0.61', "ether"));
+    });
+});
